e7fa2795b2ef7754dc5080da9340b0a1e64a47e4
jasper lorenz <jasper@lorenzmail.de>
2020-07-10 17:50:14 +0200

    Trying win32clipboard

--- src/clipboard/clipboardManager.py
+++ src/clipboard/clipboardManager.py
@@ -1,9 +1,38 @@
 from pynput import keyboard
 import clipboard
-#import win32clipboard as clipboard
+import win32clipboard as clipboard
+clipboard.OpenClipboard()
+def getClipboardData():
+  data = None
+  types = [
+    'CF_TEXT', 'CF_BITMAP', 
+    'CF_METAFILEPICT',  'CF_SYLK',
+    'CF_DIF', 'CF_TIFF', 'CF_OEMTEXT',
+    'CF_DIB', 'CF_PALETTE',  'CF_PENDATA',
+    'CF_RIFF',  'CF_WAVE',  'CF_UNICODETEXT', 
+    'CF_ENHMETAFILE',  'CF_HDROP',  'CF_LOCALE',  
+    'CF_DIBV5',  'CF_MAX', 'CF_OWNERDISPLAY',  
+    'CF_DSPTEXT',  'CF_DSPBITMAP', 'CF_DSPMETAFILEPICT', 
+    'CF_DSPENHMETAFILE'
+  ]
+  
+  the_type = None
+  for _type in types:
+    try:
+      data = clipboard.GetClipboardData(_type)
+      the_type = _type
+      print("reached")
+      break
+    except:
+      pass
+  return (data, the_type)
+print(getClipboardData())
 
 memory  = []    # Memory of the last stored clipboards
 
+
+
+exit()
 def on_press_STOP():
   listener.stop()
   exit()
