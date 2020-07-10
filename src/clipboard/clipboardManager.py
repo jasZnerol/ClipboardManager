472d3d8a4dcd190d148a7e2ffbf8fabc65b4dee3
jasper lorenz <jasper@lorenzmail.de>
2020-07-10 17:32:13 +0200

    Added 'more correct' way of implementing hotkeys

--- src/clipboard/clipboardManager.py
+++ src/clipboard/clipboardManager.py
@@ -1,38 +1,34 @@
 from pynput import keyboard
 import clipboard
+#import win32clipboard as clipboard
 
 memory  = []    # Memory of the last stored clipboard
 
-def on_press(key):
-  # Quit program
-  if key == keyboard.Key.esc:
-    listener.stop()
-    exit()
+def on_press_STOP():
+  listener.stop()
+  exit()
 
-  # Check for copy combination: CTRL + C
-  if hasattr(key, 'char') and key.char == '\x03':  
-    data = clipboard.paste()
-    if data not in memory:
-      memory.append(data)
+def on_press_COPY():
+  data = clipboard.paste()
+  if data not in memory:
+    memory.append(data)
 
-  # Check for remove key combination: CTRL + ALT + R
-  if hasattr(key, 'vk') and hasattr(key, 'char') and key.vk == 82 and key.char == None:
-    if memory != []:
+def on_press_REMOVE():
+  if memory != []:
       print("removed\t  \"" + str(memory.pop()) + "\"\tfrom clipboard")
 
-  # Check for clear key combination: CTRL + ALT + C
-  if hasattr(key, 'vk') and hasattr(key, 'char') and key.vk == 67 and key.char == None:
-    memory.clear()
-    print("memory cleared")
+def on_press_CLEAR():
+  memory.clear()
+  print("memory cleared")
 
-  # Check for print key combination: CTRL + ALT + P
-  if hasattr(key, 'vk') and hasattr(key, 'char') and key.vk == 80 and key.char == None:
-    print(memory)
+def on_press_PRINT():
+  print(memory)
 
-
-def on_release(key):
-  pass
-
-# Collect events until released
-with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
+# Add hotkeys with corresponding function
+with keyboard.GlobalHotKeys({
+  '<ctrl>+<alt>+s': on_press_STOP,
+  '<ctrl>+c': on_press_COPY,
+  '<ctrl>+<alt>+r': on_press_REMOVE,
+  '<ctrl>+<alt>+c': on_press_CLEAR,
+  '<ctrl>+<alt>+p': on_press_PRINT}) as listener: 
   listener.join()
