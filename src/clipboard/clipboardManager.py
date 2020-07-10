e8d20eb340c3d2e5d000e6a635353e8c8a17400f
jasper lorenz <jasper@lorenzmail.de>
2020-07-10 16:45:00 +0200

    Pushed 'stable' version that can do what the prototype is supposed to do

--- src/clipboard/clipboardManager.py
+++ src/clipboard/clipboardManager.py
@@ -1,34 +1,50 @@
-from pynput import keyboard 
-import win32clipboard
+from pynput import keyboard
+import clipboard
 
 
-memory = []
-win32clipboard.OpenClipboard()
-win32clipboard.EmptyClipboard()
-win32clipboard.SetClipboardText('testing 123')
-print(str(win32clipboard.GetClipboardData(win32clipboard.CF_UNICODETEXT)))
-exit()
-def on_press(key):  
-  if hasattr(key, 'char') and key.char == 'q':
-      exit()
+current = set() # Currently pressed key. Required for 
+memory  = []    # Memory of the last stored clipboard
+
+def on_press(key):
+
+  rm_memory_modifier    = { keyboard.Key.ctrl_l, keyboard.Key.alt_l, keyboard.KeyCode.from_char('r') }
+  clr_memory_modifier   = { keyboard.Key.ctrl_l, keyboard.Key.alt_l, keyboard.KeyCode.from_char('d') }
+  print_memory_modifier = { keyboard.Key.ctrl_l, keyboard.Key.alt_l, keyboard.KeyCode.from_char('p') }
   
+  # Quit program
+  if key == keyboard.Key.esc:
+    listener.stop()
+    exit()
+
   # Check for copy combination
   if hasattr(key, 'char') and key.char == '\x03': # \x03 == CTRL + C
-    try:
-      print(win32clipboard.GetClipboardData(win32clipboard.CF_TEXT))
-      memory.append(win32clipboard.GetClipboardData(win32clipboard.CF_TEXT))
-    except:
-      pass
-    print("COPY")
+    data = clipboard.paste()
+    if data not in memory:
+      memory.append(data)
 
-  # Check for paste combination
-  if hasattr(key, 'char') and key.char == 'V': # == SHIFT + V
-    if memory != []:
-      print("removed", memory.pop(), "from clipboard")
-    print("Paste")
+  # Check for remove key combination
+  if key in rm_memory_modifier:
+    current.add(key)
+    if all(k in current for k in rm_memory_modifier):
+      if memory != []:
+        print("removed\t  \"" + str(memory.pop()) + "\"\tfrom clipboard")
+
+  # Check for remove key combination
+  if key in clr_memory_modifier:
+    current.add(key)
+    if all(k in current for k in clr_memory_modifier):
+      memory.clear()
+      print("memory cleared")
+
+  # Check for print key combination
+  if key in print_memory_modifier:
+    current.add(key)
+    if all(k in current for k in print_memory_modifier):
+      print(memory)
 
 def on_release(key):
-  pass
+  current.clear()
+
 # Collect events until released
 with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
   listener.join()
