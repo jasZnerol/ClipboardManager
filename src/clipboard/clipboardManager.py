a08a578f18bcc89f6815f087796a7408868f935f
ChristophScn <christoph@hoebbel-schnell.de>
2020-07-10 20:00:28 +0200

    Implemented working win32lib for text

--- src/clipboard/clipboardManager.py
+++ src/clipboard/clipboardManager.py
@@ -1,63 +1,76 @@
-from pynput import keyboard
-import clipboard
+from functools import partial
 import win32clipboard as clipboard
-clipboard.OpenClipboard()
-def getClipboardData():
-  data = None
-  types = [
-    'CF_TEXT', 'CF_BITMAP', 
-    'CF_METAFILEPICT',  'CF_SYLK',
-    'CF_DIF', 'CF_TIFF', 'CF_OEMTEXT',
-    'CF_DIB', 'CF_PALETTE',  'CF_PENDATA',
-    'CF_RIFF',  'CF_WAVE',  'CF_UNICODETEXT', 
-    'CF_ENHMETAFILE',  'CF_HDROP',  'CF_LOCALE',  
-    'CF_DIBV5',  'CF_MAX', 'CF_OWNERDISPLAY',  
-    'CF_DSPTEXT',  'CF_DSPBITMAP', 'CF_DSPMETAFILEPICT', 
-    'CF_DSPENHMETAFILE'
-  ]
-  
-  the_type = None
-  for _type in types:
+
+import time
+
+
+
+def get_clipboard_data():
+  clipboard.OpenClipboard()
+  data = set()
+
+  format_id = clipboard.EnumClipboardFormats(0)
+  while format_id:
     try:
-      data = clipboard.GetClipboardData(_type)
-      the_type = _type
-      print("reached")
-      break
+      data.add((format_id, clipboard.GetClipboardData(format_id)))
     except:
-      pass
-  return (data, the_type)
-print(getClipboardData())
+      print(format_id)
+    format_id = clipboard.EnumClipboardFormats(format_id)
+  
+  clipboard.CloseClipboard()
+  return data
+
+def update_clipboard_data(data):
+  clipboard.OpenClipboard()
+  for format_id, entry in data:
+    try:
+      clipboard.SetClipboardData(format_id, entry)
+    except:
+      print(format_id, entry)
+
+  clipboard.CloseClipboard()
 
 memory  = []    # Memory of the last stored clipboards
 
-
-
-exit()
-def on_press_ESC():
+def on_press_esc():
   listener.stop()
   exit()
 
-def on_press_COPY():
-  data = clipboard.paste()
+def on_press_copy():
+  time.sleep(0.5)
+  data = get_clipboard_data()
   if data not in memory:
     memory.append(data)
 
-def on_press_REMOVE():
-  if memory != []:
-      print("removed\t  \"" + str(memory.pop()) + "\"\tfrom clipboard")
+def on_press_restore():
+  print("rea")
+  if not memory:
+    return
+  data = memory.pop()
+  update_clipboard_data(data)
 
-def on_press_CLEAR():
+def on_press_clear():
   memory.clear()
   print("memory cleared")
 
-def on_press_PRINT():
+def on_press_print():
   print(memory)
 
+def on_press_restore_id(id):
+  print("Restoring id {id}".format(id = id))
+
 # Add hotkeys with corresponding function
-with keyboard.GlobalHotKeys({
-  '<esc>': on_press_ESC,
-  '<ctrl>+c': on_press_COPY,
-  '<ctrl>+<alt>+r': on_press_REMOVE,
-  '<ctrl>+<alt>+c': on_press_CLEAR,
-  '<ctrl>+<alt>+p': on_press_PRINT}) as listener: 
+hotkey_map = {
+  '<esc>': on_press_esc,
+  '<ctrl>+c': on_press_copy,
+  '<ctrl>+<alt>+r': on_press_restore,
+  '<ctrl>+<alt>+c': on_press_clear,
+  '<ctrl>+<alt>+p': on_press_print
+}
+
+for id in range(10):
+  hotkey_map['<ctrl>+<alt>+{id}'.format(id = id)] = partial(on_press_restore_id, id)
+
+with keyboard.GlobalHotKeys(hotkey_map) as listener: 
   listener.join()
+
