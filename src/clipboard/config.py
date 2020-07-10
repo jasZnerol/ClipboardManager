61e50fb856acb08ce84276f3ba1c45f885198009
jasper lorenz <jasper@lorenzmail.de>
2020-07-10 21:03:48 +0200

    Added hotkeys to config

--- src/clipboard/config.py
+++ src/clipboard/config.py
@@ -1,3 +1,10 @@
-# Settings for files
-max_file_size       =   100     # Maximum file size in MB
-max_string_length   =   5000    # Maximum length of a copied string in number of chars
+# File Setting
+max_file_size       =   100               # Maximum file size in MB
+max_string_length   =   5000              # Maximum length of a copied string in number of chars
+
+# Hotkey settings
+escape_key          =   '<esc>'           # To close the clipBoardManager
+copy_key            =   '<ctrl>+c'        # Copy to clipboard and store in memory list
+restore_key         =   '<ctrl>+<alt>+r'  # ???
+clear_key           =   '<ctrl>+<alt>+c'  # Clear the entire memory list (and the clipboard?)
+print_key           =   '<ctrl>+<alt>+p'  # Print the current memory list in the console
